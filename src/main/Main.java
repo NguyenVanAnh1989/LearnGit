2d197a73b3020c8e723474fd8c49efaf95f5d974
vananh <your-correct-email@example.com>
2018-08-09 14:08:42 +0700

    commit first

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world"+ args[0]+ args[1]+ args[2]);
+	}
+
+}
---
+++ src/test/LearnGitCommit.java
@@ -0,0 +1,9 @@
+package test;
+
+public class LearnGitCommit {
+	// commit first time
+	// git develop
+	// commit my rebase
+	// commit my rebase 2
+	// this master commit
+}
